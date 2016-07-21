c2a92cadc0b9d878dc7203064833a41ccedcac0e
Igor Kvasnička <igor.kvasnicka@annotation.sk>
2016-07-21 13:56:20 +0200

    added test

--- tests/unit/services/adv-combobox-configuration-service-test.js
+++ tests/unit/services/adv-combobox-configuration-service-test.js
@@ -5,8 +5,15 @@ moduleFor('service:adv-combobox-configuration-service', 'Unit | Service | config
   // needs: ['service:foo']
 });
 
-// Replace this with your real tests.
+
 test('it exists', function(assert) {
   let service = this.subject();
   assert.ok(service);
+});
+
+test('it can change global configuration', function(assert) {
+  let service = this.subject();
+  service.setConfiguration("emptySelectionLabel", "hello world");
+
+  assert.equal(service.getEmptySelectionLabel(), "hello world");
 });
